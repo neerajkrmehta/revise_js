430658d615421a8ed3134a9e4e2e1d3b63c55bfa
Neeraj Kumar Mehta <neerajkumarmehta325@gmail.com>
2023-09-12 18:21:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let points =23
+console.log(points == 23);
+
+let a = 4;
+a++
+console.log(a);
+// control flow
+// for loop
+
+for (let i = 0; i < 10; i++) {
+    console.log("run the task",i);
+    
+}
+let j =1;
+while (j<=8) {
+    console.log("while loop",j);
+    j++
+}
+
+let bugget = 7000;
+
+if (bugget<6000) {
+    console.log("will do dinner in hotel");
+} else{
+    console.log("hello");
+}
+
+for (let i = 0; i <10; i++) {
+    if (i==5) {
+       console.log(object);
+    }
+    
+}
+
