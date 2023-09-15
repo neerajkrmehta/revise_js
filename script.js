6d594f2a5896d3ee46a71ef613861895ad76e408
Neeraj Kumar Mehta <neerajkumarmehta325@gmail.com>
2023-09-15 18:39:47 +0530

    2nd commit

--- script.js
+++ script.js
@@ -1,34 +1,146 @@
-let points =23
-console.log(points == 23);
+// let points =23
+// console.log(points == 23);
 
-let a = 4;
-a++
-console.log(a);
+// let a = 4;
+// a++
+// console.log(a);
 // control flow
 // for loop
 
-for (let i = 0; i < 10; i++) {
-    console.log("run the task",i);
+// for (let i = 0; i < 10; i++) {
+//     console.log("run the task",i);
     
-}
-let j =1;
-while (j<=8) {
-    console.log("while loop",j);
-    j++
-}
+// }
+// let j =1;
+// while (j<=8) {
+//     console.log("while loop",j);
+//     j++
+// }
 
-let bugget = 7000;
+// let bugget = 7000;
 
-if (bugget<6000) {
-    console.log("will do dinner in hotel");
-} else{
-    console.log("hello");
-}
+// if (bugget<6000) {
+//     console.log("will do dinner in hotel");
+// } else{
+//     console.log("hello");
+// }
 
-for (let i = 0; i <10; i++) {
-    if (i==5) {
-       console.log(object);
-    }
+// for (let i = 0; i <10; i++) {
+//     if (i==5) {
+//        console.log(object);
+//     }
     
-}
+// }
+// console.log("sdf");
 
+// let age = 20;
+// let weight = 54
+
+// if (age == 20) {
+//     console.log("qualify");
+// }else{
+//     console.log("Dose not qualify:fail");
+// }
+
+// // let result = age == 22 ? "QUALIFY" : "FAIL"
+
+// let result = age > 18 ? weight > 55 ? "qualify" : "weightissue" : "ageissue"
+// console.log(result);
+// let upperCase = function(str){
+//     return str.toUpperCase();
+// }
+// let lowerCase = function(str){
+//      return str.toLowerCase();
+// }
+
+// let transformer = function(str,fun){
+//     return fun(str)
+// }
+
+// console.log(transformer("HELLOW neeraj", upperCase));
+
+// let compliment = function(msg){
+//     return function(name){
+//       console.log(`${msg} and ${name}`);
+//       return true
+//     }
+// }
+
+// console.log(compliment("you are good coder")());
+
+
+// function greeting(){
+// console.log("Welcome to our Codeing Dunia");
+// }
+// setTimeout(greeting, 3000)
+
+
+// let car = {
+//     name:"honda",
+//     model:"2023",
+//     color:"black"
+// }
+// console.log(car);
+// console.log(car["name"]);
+// console.log(car.color);
+
+// let propertyName = "color"
+// console.log(car[propertyName]);
+
+// car.color = "brown"   // This is For Modification
+// console.log(car.color); 
+
+// console.log(car["color"]);
+
+
+// let obj = {
+//     prop1 : "value1",
+//     prop2 : "value2"
+// }
+
+// delete obj.prop1
+// console.log(obj.prop1);
+
+// let person = {
+//     firstName: "neeraj",
+//     lastName: "kumar",
+//     city: "bhagalpur",
+//     birthYear:2000,
+//     Education:"Electical engineer",
+//     getSummary: function(){
+//        // return `${this.firstName} ${this.lastName} live in ${this.city} and birth Year is ${this.birthYear} he is ${this.Education}`
+//        return this
+        
+//     }
+    
+// }
+// console.log(person.getSummary());
+ 
+// FOREACH
+
+// let fruits = ["apple", "banana", "gava","pineapple","orange"]
+
+// for(let i=0; i<fruits.length;i++){
+//     console.log(fruits[i]);
+// }
+
+// console.log("**********");
+
+// fruits.forEach(element => {
+//     console.log(element);
+// });
+// console.log("**********");
+
+// fruits.forEach(function(element){
+//   
+
+// console.log(element);
+// })
+
+let select = document.querySelector('a')
+
+console.log(select.getAttribute('href'));
+
+select.setAttribute('href',"https://www.youtube.com/")
+console.log(select.getAttribute('href'));
+select.innerText = "you tube link replaced"
